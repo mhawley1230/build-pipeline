ca64f774d4309a56f6ce7515cd89bc230d255124
Mike Hawley <mikehawleydev@gmail.com>
2021-10-23 15:30:19 -0400

    unit tests for Header

--- src/tests/unitTests/component/Header.test.js
+++ src/tests/unitTests/component/Header.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Header } from "../../../components/Header/Header.js";
 import { beforeEach, describe, it } from "jest-circus";
 import { shallow } from "enzyme";
+import { isTaggedTemplateExpression } from "typescript";
 
 jest.mock('../../../components/Header/Header.js', () => ({
     ...jest.requireActual('@material-ui/styles'),
@@ -16,8 +17,28 @@ beforeEach(() => {
     wrapper = shallow(<Header />);
 });
 
-test("describe Header component", () => {
+test("Header component", () => {
     it("should render", () => {
-        expect(wrapper).toHaveLength(1);;
+        expect(wrapper.find(AppBar)).toHaveLength(1);
+    });
+});
+
+describe("has elements", () => {
+    test("an appBar element", () => {
+        it("renders", () => {
+            expect(wrapper.find('.appBarClasses').to.equal(true));
+        });
+    });
+    
+    test("an Toolbar element", () => {
+        it("renders", () => {
+            expect(wrapper.find('.classes.container').to.equal(true));
+        });
+    });
+
+    test("a dropdown menu", () => {
+        it("renders", () => {
+            expect(wrapper.find('.MuiSvgIcon-root').to.equal(true));
+        });
     });
 });
